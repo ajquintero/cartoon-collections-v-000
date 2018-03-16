c36092bc201934d513a9f16bd819d7402913869a
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-16 18:48:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find{|cheese| cheese_types.include(cheese)}
+  strings.find{|cheese| cheese_types.include?(cheese)}
 end
